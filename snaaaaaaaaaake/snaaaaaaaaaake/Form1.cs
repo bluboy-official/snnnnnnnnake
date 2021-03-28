4ea0bdfcbcc9eed79b427f9df6e3bb0d691fa7a4
bluboy-official <56422076+bluboy-official@users.noreply.github.com>
2021-03-28 15:07:42 +0800

    Add files via upload

---
+++ snaaaaaaaaaake/snaaaaaaaaaake/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snaaaaaaaaaake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              
+
+        void SnakeElement(Form form, float x, float y)
+        {
+            Graphics g;
+            g = form.CreateGraphics();
+            Pen mypen = new Pen(Color.Black, 2);
+            g.DrawEllipse(mypen, x, y, 10, 10);
+            g.FillEllipse(new SolidBrush(Color.Black), x, y, 10, 10);
+            g.SmoothingMode = SmoothingMode.HighQuality;  //图片柔顺模式选择
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;//高质量
+            g.CompositingQuality = CompositingQuality.HighQuality;//再加一点
+            g.Dispose();
+            mypen.Dispose();
+            GC.Collect();
+        }
+    }
+    }
+}
